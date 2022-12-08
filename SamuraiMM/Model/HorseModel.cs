2146c0f9b316b8a917f67ca4d4ea89b5e4b9a913
mathias clausen <70236153+mathiasepc@users.noreply.github.com>
2022-12-08 09:32:29 +0100

    Repo og Model oprettet

---
+++ SamuraiMM/Model/HorseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiMM.Model
+{
+    internal class HorseModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int SamuraiID { get; set; }
+    }
+}
