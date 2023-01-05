8cfad2da0572c881904bf5e965d1c6aac70fce56
Jack Nguyen <Jaackie@live.dk>
2023-01-05 16:24:43 +0100

    HestCreate

--- SamuraiMM/Model/HorseModel.cs
+++ SamuraiMM/Model/HorseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,15 @@ namespace SamuraiMM.Model
     public class HorseModel
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string HorseRace { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Horse race is required.")]
+        [StringLength(50, ErrorMessage = "Horse race is too long.")]
+        public string? HorseRace { get; set; }
+
         public int SamuraiID { get; set; }
     }
 }
