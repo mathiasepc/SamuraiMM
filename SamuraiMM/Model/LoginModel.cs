c51716566de4ed950a37269ff4cd9739f1acf520
Jack Nguyen <Jaackie@live.dk>
2023-01-11 12:52:16 +0100

    a

--- SamuraiMM/Model/LoginModel.cs
+++ SamuraiMM/Model/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,14 @@ namespace SamuraiMM.Model
     public class LoginModel
     {
         int ID { get; set; }
+        //hvis feltet er tomt
+        //email validering
+        [Required, EmailAddress(ErrorMessage = "Email is wrong example mail: test@test.test")]
         public string Email { get; set; }
+        //hvis feltet er tomt
+        //skal være på mindst 6 karakterer
+        //kan være max 15 karakterer
+        [Required, MinLength(6, ErrorMessage = "Password must be between 6-15 characters."), StringLength(15, ErrorMessage = "Password must be between 6-15 characters.")]
         public string Password { get; set; }
     }
 }
