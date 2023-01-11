97e119393e032f0b83e2948cd4e0805d27edba09
Jack Nguyen <Jaackie@live.dk>
2023-01-11 10:49:34 +0100

    d

--- SamuraiMM/Model/LoginModel.cs
+++ SamuraiMM/Model/LoginModel.cs
@@ -9,7 +9,6 @@ namespace SamuraiMM.Model
     public class LoginModel
     {
         int ID { get; set; }
-
         public string Email { get; set; }
         public string Password { get; set; }
     }
